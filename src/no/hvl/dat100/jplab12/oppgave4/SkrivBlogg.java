2c1b834617f2e6c03197d591bb3b60561a99956b
cerb <cerb@DESKTOP-MQ63F19.lan>
2019-10-31 10:31:59 +0100

    Skrivblogg kanskje ferdig

--- src/no/hvl/dat100/jplab12/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab12/oppgave4/SkrivBlogg.java
@@ -11,7 +11,30 @@ public class SkrivBlogg {
 	private static String MAPPE = System.getProperty("user.dir") + "/src/no/hvl/dat100/jplab12/tests/";
 
 	public static boolean skriv(Blogg samling, String filnavn) {
+	
+		String skrivtilfil = filnavn;
+		String skriverier = samling.toString();
+		PrintWriter skriver;
+		try {
+		
+			skriver = new PrintWriter(skrivtilfil);
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+			return false;
+		}
+		for(int i = 0; i<samling.getAntall(); i++) {
+			
+		
+			
+			
+		} 			skriver.println(skriverier);
+		skriver.close();
+		return true;
+		
+		
+		
 
-		throw new UnsupportedOperationException(TODO.method());
+
 	}
 }
